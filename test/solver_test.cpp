d5078ca58852bc84ac16ffef40a14a911de137bc
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2024-05-20 23:01:35 +0300

    solver_test implementation continued

--- test/solver_test.cpp
+++ test/solver_test.cpp
@@ -1,19 +1,29 @@
 #include <gtest/gtest.h>
 
 #include <iostream>
+#include <memory>
 
 #include <src/Grid/Defines.h>
+#include <src/Solver/Solver.hpp>
+
 #include <src/GridFactory.hpp>
+
+#include <src/ConcreteProblem/ProblemFactory.hpp>
+
 
 using namespace EqSolver;
 using namespace EqSolver::Grid;
+using namespace EqSolver::Properties;
 
-TEST(Grid, ctor)
+TEST(Solver, splitX)
 {
   UniformGrid2D grid{
       GridFactory::CreateGridFromStep(
           Box{0.0, 1.0, 0.0, 1.0},
           Steps{0.1, 0.1})};
+
+   std::shared_ptr<Properties::Fields>
+     properties{std::make_shared<Properties::Fields>(grid, Capacity{}, Conductivity{})};
 }
 
 int main(int argc, char **argv)
