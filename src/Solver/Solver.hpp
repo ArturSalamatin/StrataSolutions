a5420f19f80d61c9c894c7d6483d2335847631ba
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2024-05-20 22:54:05 +0300

    index type mismatch fixed

--- src/Solver/Solver.hpp
+++ src/Solver/Solver.hpp
@@ -50,8 +50,8 @@ namespace EqSolver
                   LaplaceTerm(
                       serial_nodes,                // number of matricies
                       Eigen::SparseMatrix<float_t>{// ctor per matrix
-                                                   matrix_size,
-                                                   matrix_size})
+                                                   ptrdiff_t(matrix_size),
+                                                   ptrdiff_t(matrix_size)})
             {
                 for (auto &m : LaplaceTerm)
                 {
@@ -69,9 +69,10 @@ namespace EqSolver
         struct SplitX
             : public BaseSplit
         {
+            template <typename Grid_t>
             SplitX(
                 std::shared_ptr<Properties::Fields> properties,
-                const Grid::UniformGrid2D &grid)
+                const Grid_t &grid)
                 : BaseSplit{
                       properties,
                       grid.X_nodes.size(),
@@ -84,21 +85,21 @@ namespace EqSolver
             template <typename Grid_t>
             void FillLaplaceTerm(const Grid_t &grid)
             {
-                conductivity_y_bounds{
+                Conductivity_f conductivity_y_bounds{
                     set_conductivity_y_bounds(properties->conductivity_f, grid)};
 
                 const auto &gr_y = grid.Y_nodes;
 
 #pragma omp parallel for
-                for (size_t m_id = 0; m_id < LaplaceTerm.size(); ++m_id)
+                for (ptrdiff_t m_id = 0; m_id < ptrdiff_t(LaplaceTerm.size()); ++m_id)
                 {
-                    const auto &matrix = LaplaceTerm[m_id];
+                    auto &matrix = LaplaceTerm[m_id];
                     std::vector<Eigen::Triplet<float_t>> tripletList;
                     tripletList.reserve(matrix.rows() * 3ull - 2ull);
 
-                    tripletList.emplace_back(0ull, 0ull, conductivity_y_bounds(m_id, 0) / gr_y.step(0));
-                    tripletList.emplace_back(0ull, 1ull, conductivity_y_bounds(m_id, 0) / gr_y.step(0));
-                    for (size_t row = 1; row < matrix.rows() - 1; ++row)
+                    tripletList.emplace_back(0, 0, conductivity_y_bounds(m_id, 0) / gr_y.step(0));
+                    tripletList.emplace_back(0, 1, conductivity_y_bounds(m_id, 0) / gr_y.step(0));
+                    for (ptrdiff_t row = 1; row < ptrdiff_t(matrix.rows()) - 1; ++row)
                     {
                         tripletList.emplace_back(row, row - 1,
                                                  conductivity_y_bounds(m_id, row) / gr_y.step(row - 1));
@@ -108,7 +109,7 @@ namespace EqSolver
                         tripletList.emplace_back(row, row + 1,
                                                  conductivity_y_bounds(m_id, row + 1) / gr_y.step(row));
                     }
-                    size_t end = matrix.rows() - 1;
+                    ptrdiff_t end = ptrdiff_t(matrix.rows()) - 1;
                     tripletList.emplace_back(end, end - 1,
                                              conductivity_y_bounds(m_id, end + 1) / gr_y.step(end));
                     tripletList.emplace_back(end, end,
@@ -128,17 +129,18 @@ namespace EqSolver
                     grid.Y_nodes.size() + 1};
 
 #pragma omp parallel for
-                for (size_t i = 0; i < conductivity_y_bounds.rows(); ++i)
+                for (ptrdiff_t i = 0; i < conductivity_y_bounds.rows(); ++i)
                 {
                     conductivity_y_bounds(i, 0) =
                         conductivity_nodes(i, 0);
-                    for (size_t j = 1; j < conductivity_y_bounds.cols() - 1; ++j)
+                    for (ptrdiff_t j = 1; j < conductivity_y_bounds.cols() - 1; ++j)
                     {
                         conductivity_y_bounds(i, j) =
                             (conductivity_nodes(i, j - 1) + conductivity_nodes(i, j)) / 2.0;
                     }
+                    ptrdiff_t end = conductivity_y_bounds.cols() - 1;
                     conductivity_y_bounds(i, end) =
-                        conductivity_nodes(i, end);
+                        conductivity_nodes(i, end-1);
                 }
                 return conductivity_y_bounds;
             }
@@ -147,9 +149,10 @@ namespace EqSolver
         struct SplitY
             : public BaseSplit
         {
+            template <typename Grid_t>
             SplitY(
                 std::shared_ptr<Properties::Fields> properties,
-                const Grid::UniformGrid2D &grid)
+                const Grid_t &grid)
                 : BaseSplit{properties, grid.Y_nodes.size(), grid.X_nodes.size()}
             {
                 FillLaplaceTerm(grid);
@@ -165,17 +168,17 @@ namespace EqSolver
                 const auto &gr_x = grid.X_nodes;
 
 #pragma omp parallel for
-                for (size_t m_id = 0; m_id < LaplaceTerm.size(); ++m_id)
+                for (ptrdiff_t m_id = 0; m_id < ptrdiff_t(LaplaceTerm.size()); ++m_id)
                 {
-                    const auto &matrix = LaplaceTerm[m_id];
+                    auto &matrix = LaplaceTerm[m_id];
                     std::vector<Eigen::Triplet<float_t>> tripletList;
                     tripletList.reserve(matrix.rows() * 3ull - 2ull);
 
-                    tripletList.emplace_back(0ull, 0ull,
+                    tripletList.emplace_back(0, 0,
                                              conductivity_x_bounds(0, m_id) / gr_x.step(0));
-                    tripletList.emplace_back(0ull, 1ull,
+                    tripletList.emplace_back(0, 1,
                                              conductivity_x_bounds(1, m_id) / gr_x.step(0));
-                    for (size_t row = 1; row < matrix.rows() - 1; ++row)
+                    for (ptrdiff_t row = 1; row < ptrdiff_t(matrix.rows()) - 1; ++row)
                     {
                         tripletList.emplace_back(row, row - 1,
                                                  conductivity_x_bounds(row, m_id) / gr_x.step(row - 1));
@@ -185,7 +188,7 @@ namespace EqSolver
                         tripletList.emplace_back(row, row + 1,
                                                  conductivity_x_bounds(row + 1, m_id) / gr_x.step(row));
                     }
-                    size_t end = matrix.rows() - 1;
+                    ptrdiff_t end = ptrdiff_t(matrix.rows()) - 1;
                     tripletList.emplace_back(end, end - 1,
                                              conductivity_x_bounds(end, m_id) / gr_x.step(end));
                     tripletList.emplace_back(end, end,
@@ -205,17 +208,18 @@ namespace EqSolver
                     grid.Y_nodes.size()};
 
 #pragma omp parallel for
-                for (size_t j = 0; j < conductivity_x_bounds.cols(); ++j)
+                for (ptrdiff_t j = 0; j < ptrdiff_t(conductivity_x_bounds.cols()); ++j)
                 {
                     conductivity_x_bounds(0, j) =
                         conductivity_nodes(0, j);
-                    for (size_t i = 1; i < conductivity_x_bounds.rows() - 1; ++i)
+                    for (ptrdiff_t i = 1; i < ptrdiff_t(conductivity_x_bounds.rows()) - 1; ++i)
                     {
                         conductivity_x_bounds(i, j) =
                             (conductivity_nodes(i - 1, j) + conductivity_nodes(i, j)) / 2.0;
                     }
+                    ptrdiff_t end = conductivity_x_bounds.rows() - 1;
                     conductivity_x_bounds(end, j) =
-                        conductivity_nodes(end, j);
+                        conductivity_nodes(end-1, j);
                 }
 
                 return conductivity_x_bounds;
