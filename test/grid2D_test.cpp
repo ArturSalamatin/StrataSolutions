f8b212c40837798e7dfca1ca5c2b5e2954ba015d
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2024-05-21 15:07:09 +0300

    tests fixed

--- test/grid2D_test.cpp
+++ test/grid2D_test.cpp
@@ -1,34 +1,28 @@
 #include <gtest/gtest.h>
 
 #include <iostream>
-#include <memory>
-
-#include <src/Grid/Defines.h>
-#include <src/Solver/Solver.hpp>
-
+#include <src/Grid/UniformGrid2D.h>
 #include <src/GridFactory.hpp>
-
-#include <src/ConcreteProblem/ProblemFactory.hpp>
 
 using namespace EqSolver;
 using namespace EqSolver::Grid;
-using namespace EqSolver::Properties;
-using namespace EqSolver::SplittingMethod;
 
-TEST(Solver, splitX)
+TEST(Grid2D, ctor)
 {
   const double tol = 1E-8;
 
   UniformGrid2D grid{
       GridFactory::CreateGridFromStep(
           Box{0.0, 1.0, 0.0, 1.0},
-          Steps{0.11, 0.11})};
+          Steps{0.065, 0.11})};
 
+  // test steps of 1D grids
   for (size_t i = 0; i < grid.X_nodes.size(); ++i)
-    ASSERT_NEAR(grid.X_nodes.step(i), 0.1, tol);
+    ASSERT_NEAR(grid.X_nodes.step(i), 0.0625, tol);
   for (size_t j = 0; j < grid.Y_nodes.size(); ++j)
     ASSERT_NEAR(grid.Y_nodes.step(j), 0.1, tol);
 
+  // test volumes of internal cells
   for (size_t i = 1; i < grid.X_nodes.size() - 1; ++i)
     for (size_t j = 1; j < grid.Y_nodes.size() - 1; ++j)
     {
@@ -38,6 +32,7 @@ TEST(Solver, splitX)
           tol);
     }
 
+  // test volumes of boundary cells except corners
   for (size_t i = 0; i < grid.X_nodes.size(); i += grid.X_nodes.size() - 1)
     for (size_t j = 1; j < grid.Y_nodes.size() - 1; ++j)
     {
@@ -54,7 +49,8 @@ TEST(Solver, splitX)
           grid.X_nodes.step(i) * grid.Y_nodes.step(j) / 2.0,
           tol);
     }
-    
+
+  // test volumes of corner cells
   for (size_t j = 0; j < grid.Y_nodes.size(); j += grid.Y_nodes.size() - 1)
     for (size_t i = 0; i < grid.X_nodes.size(); i += grid.X_nodes.size() - 1)
     {
@@ -63,16 +59,6 @@ TEST(Solver, splitX)
           grid.X_nodes.step(i) * grid.Y_nodes.step(j) / 4.0,
           tol);
     }
-
-
-  std::shared_ptr<Properties::Fields>
-      properties{
-          std::make_shared<Properties::Fields>(
-              grid,
-              Capacity{},
-              Conductivity{})};
-
-  SplitX splitx{properties, grid};
 }
 
 int main(int argc, char **argv)
