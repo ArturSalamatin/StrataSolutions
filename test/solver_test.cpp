4528720a7b060f5a919e7e704c399412632e3811
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2024-05-21 15:13:41 +0300

    test fixed

--- test/solver_test.cpp
+++ test/solver_test.cpp
@@ -24,47 +24,6 @@ TEST(Solver, splitX)
           Box{0.0, 1.0, 0.0, 1.0},
           Steps{0.11, 0.11})};
 
-  for (size_t i = 0; i < grid.X_nodes.size(); ++i)
-    ASSERT_NEAR(grid.X_nodes.step(i), 0.1, tol);
-  for (size_t j = 0; j < grid.Y_nodes.size(); ++j)
-    ASSERT_NEAR(grid.Y_nodes.step(j), 0.1, tol);
-
-  for (size_t i = 1; i < grid.X_nodes.size() - 1; ++i)
-    for (size_t j = 1; j < grid.Y_nodes.size() - 1; ++j)
-    {
-      ASSERT_NEAR(
-          grid.cell_volume(i, j),
-          grid.X_nodes.step(i) * grid.Y_nodes.step(j),
-          tol);
-    }
-
-  for (size_t i = 0; i < grid.X_nodes.size(); i += grid.X_nodes.size() - 1)
-    for (size_t j = 1; j < grid.Y_nodes.size() - 1; ++j)
-    {
-      ASSERT_NEAR(
-          grid.cell_volume(i, j),
-          grid.X_nodes.step(i) * grid.Y_nodes.step(j) / 2.0,
-          tol);
-    }
-  for (size_t j = 0; j < grid.Y_nodes.size(); j += grid.Y_nodes.size() - 1)
-    for (size_t i = 1; i < grid.X_nodes.size() - 1; ++i)
-    {
-      ASSERT_NEAR(
-          grid.cell_volume(i, j),
-          grid.X_nodes.step(i) * grid.Y_nodes.step(j) / 2.0,
-          tol);
-    }
-    
-  for (size_t j = 0; j < grid.Y_nodes.size(); j += grid.Y_nodes.size() - 1)
-    for (size_t i = 0; i < grid.X_nodes.size(); i += grid.X_nodes.size() - 1)
-    {
-      ASSERT_NEAR(
-          grid.cell_volume(i, j),
-          grid.X_nodes.step(i) * grid.Y_nodes.step(j) / 4.0,
-          tol);
-    }
-
-
   std::shared_ptr<Properties::Fields>
       properties{
           std::make_shared<Properties::Fields>(
